38a3a3aabe872adfe667c316015325809d9f81ee
Luna Partida <lunapartida93@icloud.com>
2022-10-27 20:40:22 -0700

    removed image

--- js/main.js
+++ js/main.js
@@ -76,7 +76,7 @@ image_array = [
 function get_random_image() {
   random_index = Math.floor(Math.random() * image_array.length);
   selected_image = image_array[random_index];
-  document.getElementById("image_shower").src = `/image/${selected_image}`;
+  document.getElementById("image_shower").src = `${selected_image}`;
 }
 
 //audio for button click
